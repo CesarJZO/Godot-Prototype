e2fb4a44b230e812d7949361e64096afbe157cd6
César Zárate <cesarjzo@outlook.com>
2023-09-16 20:29:02 -0600

    Add user input to sprite

--- MySprite.cs
+++ MySprite.cs
@@ -4,14 +4,23 @@ namespace Prototype;
 
 public sealed partial class MySprite : Sprite2D
 {
-    private int _speed = 400;
+    [Export] private int speed = 400;
     private float _angularSpeed = Mathf.Pi;
 
     public override void _Process(double delta)
     {
-        Rotation += _angularSpeed * (float)delta;
+        float direction = 0f;
 
-        Vector2 velocity = Vector2.Up.Rotated(Rotation) * _speed;
+        if (Input.IsActionPressed("ui_right"))
+            direction += 1f;
+        else if (Input.IsActionPressed("ui_left"))
+            direction -= 1f;
+
+        Rotation += _angularSpeed * direction * (float)delta;
+
+        Vector2 velocity = Vector2.Zero;
+        if (Input.IsActionPressed("ui_up"))
+            velocity = Vector2.Up.Rotated(Rotation) * speed;
 
         Position += velocity * (float)delta;
     }
